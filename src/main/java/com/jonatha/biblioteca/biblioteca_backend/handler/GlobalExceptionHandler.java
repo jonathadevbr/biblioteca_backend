b96453c706a6615c1c37ee31b87e00d457605f57
Jonatha Vianna <jhonatavianna191@gmail.com>
2026-06-25 20:40:47 -0300

    feat: adiciona tratamento de exceções para validação de argumentos em métodos

--- src/main/java/com/jonatha/biblioteca/biblioteca_backend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/handler/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.jonatha.biblioteca.biblioteca_backend.handler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +23,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<String> handleResourceConflictException(ConflictException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        
+        // Pega todos os erros e coloca no map: { "campo": "mensagem de erro" }
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
