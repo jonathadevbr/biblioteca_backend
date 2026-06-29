c0d40b6fc35975171c64db0e28e3120a8c46c85e
Jonatha Vianna <jhonatavianna191@gmail.com>
2026-06-29 19:27:53 -0300

    refactor: remove validação de campo obrigatório para nome da categoria

--- src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/request/categoria/CategoriaUpdateRequestDTO.java
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/request/categoria/CategoriaUpdateRequestDTO.java
@@ -1,11 +1,9 @@
 package com.jonatha.biblioteca.biblioteca_backend.dto.request.categoria;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CategoriaUpdateRequestDTO(
 
-    @NotBlank(message = "O nome da categoria é obrigatório.")
     @Size(min = 5, max = 255, message = "O nome da categoria deve ter entre 5 e 255 caracteres")
     String nome,
 
