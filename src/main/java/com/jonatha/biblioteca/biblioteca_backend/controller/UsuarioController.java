8afd74623e5e9ffcd416eea6c4406b14c6df86d2
Jonatha Vianna <jhonatavianna191@gmail.com>
2026-08-04 15:50:00 -0300

    refactor: remove importação de PageRequest no UsuarioController

--- src/main/java/com/jonatha/biblioteca/biblioteca_backend/controller/UsuarioController.java
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/controller/UsuarioController.java
@@ -19,12 +19,10 @@ import jakarta.validation.Valid;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
