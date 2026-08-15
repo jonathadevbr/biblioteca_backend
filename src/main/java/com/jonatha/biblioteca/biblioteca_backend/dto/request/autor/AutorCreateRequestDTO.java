4f1296249d119a05f4c222b17201dd3804f50e86
Jonatha Vianna <jonatha.vianna01@gmail.com>
2026-08-15 17:29:31 -0300

    refactor: ajusta validações e remove método desnecessário na classe AutorCreateRequestDTO

--- src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/request/autor/AutorCreateRequestDTO.java
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/request/autor/AutorCreateRequestDTO.java
@@ -1,6 +1,5 @@
 package com.jonatha.biblioteca.biblioteca_backend.dto.request.autor;
 
-import com.jonatha.biblioteca.biblioteca_backend.model.Autor;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,20 +7,11 @@ import jakarta.validation.constraints.Size;
 public record AutorCreateRequestDTO(
 
     @NotBlank(message = "O nome do autor é obrigatório.")
-    @Size(min = 1, max = 255, message = "O nome do autor deve ter entre 1 e 255 caracteres")
+    @Size(max = 255, message = "O nome do autor deve ter no máximo 255 caracteres")
     String nome,
 
-    @NotBlank(message = "A nacionalidade do autor é obrigatório.")
+    @NotBlank(message = "A nacionalidade do autor é obrigatória.")
     @Size(min = 5, max = 255, message = "A nacionalidade do autor deve ter entre 5 e 255 caracteres")
     String nacionalidade
 
-) {
-    public Autor createAutor() {
-        Autor autor = new Autor();
-
-        autor.setNome(this.nome);
-        autor.setNacionalidade(this.nacionalidade);
-
-        return autor;
-    }
-}
+) { }
