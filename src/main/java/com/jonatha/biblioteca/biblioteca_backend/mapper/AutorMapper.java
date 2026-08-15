15cf2eb36eeb13bd78734ab31795e2297768a76d
Jonatha Vianna <jonatha.vianna01@gmail.com>
2026-08-15 17:29:22 -0300

    feat: adiciona classe AutorMapper para conversão entre DTO e entidade Autor

---
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/mapper/AutorMapper.java
@@ -0,0 +1,29 @@
+package com.jonatha.biblioteca.biblioteca_backend.mapper;
+
+import com.jonatha.biblioteca.biblioteca_backend.dto.request.autor.AutorCreateRequestDTO;
+import com.jonatha.biblioteca.biblioteca_backend.dto.response.AutorResponseDTO;
+import com.jonatha.biblioteca.biblioteca_backend.model.Autor;
+
+public class AutorMapper {
+    // ENTRADA de dados
+    public static Autor toEntity(AutorCreateRequestDTO request) {
+        if (request == null) return null;
+
+        Autor autor = new Autor();
+        autor.setNome(request.nome());
+        autor.setNacionalidade(request.nacionalidade());
+        return autor;
+    }
+
+    // SÁIDA de dados
+    public static AutorResponseDTO toDTO(Autor autor) {
+        if (autor == null) return null;
+
+        return new AutorResponseDTO(
+            autor.getId(),
+            autor.getNome(),
+            autor.getNacionalidade()
+        );
+    }
+
+}
