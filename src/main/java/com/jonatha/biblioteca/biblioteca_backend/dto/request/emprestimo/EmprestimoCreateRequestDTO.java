1f19612b186d276ec4436ac0fef61601dd62f59c
Jonatha Vianna <jonatha.vianna01@gmail.com>
2026-09-08 21:50:07 -0300

    fix(EmprestimoCreateRequestDTO): alterar anotação de validação do ID do livro para NotEmpty

--- src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/request/emprestimo/EmprestimoCreateRequestDTO.java
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/request/emprestimo/EmprestimoCreateRequestDTO.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 
 import com.jonatha.biblioteca.biblioteca_backend.enums.StatusEmprestimo;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record EmprestimoCreateRequestDTO(
@@ -13,7 +14,7 @@ public record EmprestimoCreateRequestDTO(
     @NotNull(message = "O ID do usuário é obrigatório.")
     UUID idUsuario,
 
-    @NotNull(message = "O ID do livro é obrigatório.")
+    @NotEmpty (message = "O ID do livro é obrigatório.")
     List<UUID> idsLivro,
 
     @NotNull(message = "A data inicial do emprestimo é obrigatória.")
