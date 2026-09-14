dccbf73efa988a6eb9c92c2622b3674dd421c636
Jonatha Vianna <jonatha.vianna01@gmail.com>
2026-09-14 20:32:18 -0300

    fix(EmprestimoResponseDTO): adicionar formatação de data para campos de empréstimo

--- src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/response/EmprestimoResponseDTO.java
+++ src/main/java/com/jonatha/biblioteca/biblioteca_backend/dto/response/EmprestimoResponseDTO.java
@@ -4,14 +4,18 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jonatha.biblioteca.biblioteca_backend.enums.StatusEmprestimo;
 
 public record EmprestimoResponseDTO(
     UUID id,
     UsuarioResponseDTO usuario,
     List<LivroResponseDTO> livros,
+    @JsonFormat(pattern = "dd/MM/yyyy")
     LocalDate dataEmprestimo,
+    @JsonFormat(pattern = "dd/MM/yyyy")
     LocalDate dataPrevisaoDevolucao,
+    @JsonFormat(pattern = "dd/MM/yyyy")
     LocalDate dataDevolucaoReal,
     StatusEmprestimo status
 ) { }
